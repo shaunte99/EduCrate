b33b74c09f7e4bb68798524ca0760a12d65516f4
shaunte99 <ksmatsetela99@gmail.com>
2025-10-06 20:13:38 +0200

    Create about.js

---
+++ scripts/about.js
@@ -0,0 +1,19 @@
+// Stats Count Up Animation
+const counters = document.querySelectorAll('.count');
+const speed = 200; // lower is faster
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    const increment = target / speed;
+
+    if(count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 20);
+    } else {
+      counter.innerText = target.toLocaleString();
+    }
+  };
+  updateCount();
+});
